1c6190e414f0e76ac748f221b1b1af55418a6829
Benito Asensio <magnumetal@gmail.com>
2022-08-04 11:51:54 +0200

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,51 @@
+// servidor de express
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sokets');
+
+
+class Server {
+    constructor() {
+
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // HTTP server
+        this.server = http.createServer(this.app);
+
+        // Configuraciones de sokets
+        this.io = socketio(this.server, { /* configuraciones */ }) ;
+
+    }
+    middlewares() {
+        // Desplegar el directorio público
+        this.app.use(express.static(path.resolve( __dirname, '../public' )));
+    }
+
+    configurarSockets() {
+        new Sockets( this.io );
+    }
+    
+    execute() {
+
+        // Inicializar Middelwares
+        this.middlewares();
+
+        // Inicializara sokets
+        this.configurarSockets();
+
+        // Inicializar server
+        this.server.listen(this.port, () => {
+         console.log("Server corriendo en puerto:", this.port );
+        });
+
+    }
+
+}
+
+
+
+
+module.exports = Server;
---
+++ models/sokets.js
@@ -0,0 +1,27 @@
+
+
+class Sockets {
+
+    constructor( io ) {
+        this.io = io;
+        this.SocketEvents();
+    }
+
+    SocketEvents() {
+        // On connection
+        this.io.on("connection", (socket) => {
+ 
+        // escuchar evento: mensaje-to-server
+            socket.on("mensaje-to-server", (data) => {
+                console.log(data);
+                this.io.emit('mensaje-from-server', data);
+            });
+
+        });
+
+    }
+}
+
+
+module.exports = Sockets;
+
